460310ca04c0af24917e17bc33c72f0b12ba964a
Rob Moore <me@robdmoore.id.au>
2016-02-17 23:15:22 +0800

    Enabled cors

--- MvcAppWithFormsAuth/App_Start/WebApiConfig.cs
+++ MvcAppWithFormsAuth/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace MvcAppWithFormsAuth
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors();
         }
     }
 }
