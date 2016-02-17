7c2bd251752274bddbd913b433a430ade7632911
Rob Moore <me@robdmoore.id.au>
2016-02-17 22:37:42 +0800

    Added scope and client

---
+++ MvcAppWithFormsAuth/App_Start/Scopes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core.Models;
+
+namespace MvcAppWithFormsAuth
+{
+    static class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope>
+        {
+            new Scope
+            {
+                Name = "reporting_api"
+            }
+        };
+        }
+    }
+}
